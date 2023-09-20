ef6691bd0b77f8822bac1cc23fab676cd1a07bf3
Geoffrey Ihsan <geoffreyihsan@geoffrey-4.local>
2023-09-20 13:30:35 +0700

    15th Commit on Production Development (Update Contests landing page & remove (airline news & trade talk) stories on Homepage deployment on 20th of September)

--- components/ContestPost/ContestPost.js
+++ components/ContestPost/ContestPost.js
@@ -4,7 +4,7 @@ import styles from './ContestPost.module.scss'
 
 let cx = classNames.bind(styles)
 
-export default function ContestPost({ title, uri, featuredImage, date }) {
+export default function ContestPost({ title, uri, featuredImage }) {
   return (
     <article className={cx('component')}>
       {featuredImage && (
@@ -22,12 +22,6 @@ export default function ContestPost({ title, uri, featuredImage, date }) {
         <a href={uri}>
           <h2 className={cx('title')}>{title}</h2>
         </a>
-      </div>
-      <div className={cx('date-wrapper')}>
-        <h2 className={cx('date')}>
-          {'Contest Date: '}
-          {date}
-        </h2>
       </div>
       <div className={cx('content-wrapper-image')}>
         <div className={cx('border-bottom')}></div>
