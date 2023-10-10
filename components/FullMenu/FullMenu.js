3bfc9378421fba283559ae928ce89f98dbdbac96
Geoffrey Ihsan <geoffreyihsan@geoffrey-3.local>
2023-10-10 13:56:27 +0700

    25th Commit on Production Development (Feature Well fix & Luxe List deployment on 10th of October)

--- components/FullMenu/FullMenu.js
+++ components/FullMenu/FullMenu.js
@@ -219,6 +219,10 @@ l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
                   }
                 />
               </a>
+              {console.log(
+                featureMenuItems[0]?.menu?.node?.luxeListLogoMenu?.mainLogo
+                  ?.mediaItemUrl,
+              )}
             </nav>
           </div>
           <div className={cx('fourth-wrapper')}>
