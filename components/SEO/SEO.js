98828287e0c39aa9b7b25c26310cb015568d99c8
Geoffrey Ihsan <geoffreyihsan@geoffrey-3.local>
2023-10-30 16:46:50 +0700

    38th Commit on Production Development (Typography Cloud changes deployment on 30th of October)

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -143,7 +143,7 @@ export default function SEO({ title, description, imageUrl, url, focuskw }) {
         <link
           rel="stylesheet"
           type="text/css"
-          href="https://cloud.typography.com/7429004/6477832/css/fonts.css"
+          href="https://cloud.typography.com/7429004/7933832/css/fonts.css"
         />
 
         {/* Google Fonts */}
