73f65171e1cd41aabbfbdb2ed547113a19c6dde1
DestinAsian <geoffreyihsan@geoffrey-4.local>
2024-02-05 11:46:07 +0700

    Production deployment on 5th February 2024

--- components/ContentWrapperAdvertorial/ContentWrapperAdvertorial.js
+++ components/ContentWrapperAdvertorial/ContentWrapperAdvertorial.js
@@ -57,7 +57,7 @@ export default function ContentWrapperAdvertorial({ content, children }) {
 
   return (
     <article className={cx('component')}>
-      <div className={cx('content-wrapper')} dangerouslySetInnerHTML={{ __html: transformedContent ?? '' }} />
+      <div className={cx('content-wrapper')} dangerouslySetInnerHTML={{ __html: content ?? '' }} />
       {children}
     </article>
   );
